8a7d1aaedd6314b903033df998e7879c26ab5ecd
amdadul haque <amdadulhaque601671@gmail.com>
2026-06-26 12:18:25 +0600

    We have created server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const http = require('http');
-
 const app = {};
 
-app.config = {};
+app.config = {
+    port: 3000
+} 
 
 app.createServer = () => {
-    const server = http.createServer(handleReqRes);
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Server is running port on ${app.config.port}`);
+    });
 }
 
 app.handleReqRes = (req, res) => {
-    res.send('Hello World');
+    res.end("Hello world");
 }
+
+app.createServer()
